6c3a0fa0562f2cbe9b9e04aa9e41ec8692f4a6e6
장재완 <wkdwodhks7@naver.com>
2019-12-02 16:44:13 +0900

    MergeSort 완성

--- C++/Test/01MergeSort.cpp
+++ C++/Test/01MergeSort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 
 vector<int>* MergeSortFunc(vector<int>* vecList);
+void TestPrint(vector<int>* vec);
 
 
 void main()
@@ -11,11 +12,11 @@ void main()
 	int iVal = 0;
 	int iInsert = 0;
 	vector<int>* vecList = new vector<int>;
-	vector<int>* test = new vector<int>;
-	vecList->push_back(10);
-	vecList->push_back(15);
-	vecList->push_back(9);
-	vecList->push_back(25);
+	vecList->push_back(4);
+	vecList->push_back(3);
+	vecList->push_back(2);
+	vecList->push_back(1);
+	vecList->push_back(30);
 
 
 	while (true)
@@ -25,7 +26,7 @@ void main()
 		cout << "2. 출력" << endl;
 		cout << "3. 병합정렬" << endl;
 		cin >> iVal;
-
+	
 		switch (iVal)
 		{
 		case 1:
@@ -34,8 +35,8 @@ void main()
 			cin >> iInsert;
 			vecList->push_back(iInsert);
 			break;
-
-
+	
+	
 		case 2:
 			for (auto it = vecList->begin(); it != vecList->end(); ++it)
 				cout << (*it) << ", ";
@@ -58,75 +59,100 @@ vector<int>* MergeSortFunc(vector<int>* vecList)
 	auto it	= vecList->begin();
 	int iVecSize	= vecList->size();
 	int iHalf		= iVecSize / 2;
-	int temp		= 0;
-
 	left.reserve(iHalf + 1);
 	right.reserve(iHalf);
 
 
 	// ------------------------------------------------ 분할 과정
-	// 벡터 크기가 2개보다 작거나 같을때
+	// 최소단위 까지 나뉘어 졌을때.
 	if (iVecSize <= 2)
-	{	// 벡터 크기가 0또는 1이면 
+	{	 
 		if (0 == iVecSize || 1 == iVecSize)
 			return vecList;
 
-		// 2개의 값 스왑
-		if (*it < *(it+1))
+		if (*it > *(it + 1))
+		{
 			*it ^= *(it + 1) ^= *it ^= *(it + 1);
+			return vecList;
+		}
+		return vecList;
 	}
-	// copy 사용시 왜 xutil 에러가 뜨는지 모르겠음.
-	//else if (iHalf & 1)
-	//{	// 홀수
-	//	//   begin()에서						(iHalf + 1)만큼					left로 복사
-	//	copy(vecList->begin()				, vecList->begin() + (iHalf + 1), &left );
-	//	copy(vecList->begin() + (iHalf + 2)	, vecList->end() - 1			, &right);
-	//}
-	//else
-	//{	// 짝수
-	//	copy(vecList->begin()				, vecList->begin() + iHalf		, &left );
-	//	copy(vecList->begin() + (iHalf+1)	, vecList->end() - 1			, &right);
-	//}
-
-
-	// 현재 push_back으로 첫값은 들어오는데 두번째 값부터 안들어옴.
-	else if (iHalf & 1)
+	else if (iVecSize & 1)
 	{	// 홀수
 		for (auto it2 = it				; it2 != (it + iHalf + 1)	; ++it2)
 			left.push_back(*it2);
-	
-		for (auto it2 = it + (iHalf + 2); it2 != vecList->end()		; ++it2)
+
+		for (auto it2 = it + (iHalf + 1); it2 != vecList->end()		; ++it2)
 			right.push_back(*it2);
 	}
 	else
 	{	// 짝수
 		for (auto it2 = it				; it2 != it + iHalf			; ++it2)
 			left.push_back((*it2));
-		system("pause");
 
 		for (auto it2 = it + iHalf		; it2 != vecList->end()		; ++it2)
 			right.push_back((*it2));
 	}
+	left  = *(MergeSortFunc(&left ));
+	right = *(MergeSortFunc(&right));
+	TestPrint(&left);
+	TestPrint(&right);
 
 
 	// ------------------------------------------------- 합병 과정
-	left  = *(MergeSortFunc(&left ));
-	right = *(MergeSortFunc(&right));
+	int iLeftCount = 0;
+	int iRightCount = 0;
+	
+	
 	vecList->clear();
-	for (auto iter = left.begin(); iter != left.begin(); ++iter)
+	while (left.size() != iLeftCount)
 	{
-		for (auto iter2 = right.begin(); iter2 != right.begin(); ++iter2)
+		if (left[iLeftCount] < right[iRightCount])
 		{
-			if ((*iter) < (*iter2))
-			{
-				vecList->push_back((*iter));
-				break;
-			}
-			else
-				vecList->push_back((*iter2));
+			vecList->push_back(left[iLeftCount]);
+			cout << left[iLeftCount] << " ";
+			++iLeftCount;
+			continue;
+		}
+		else
+		{
+			vecList->push_back(right[iRightCount]);
+			cout << right[iRightCount] << " ";
+			++iRightCount;
+		}
+		if (iRightCount >= right.size())
+			break;
+	}
+
+	
+	if (left.size() != iLeftCount)
+	{
+		for (int i = iLeftCount; i < left.size(); ++i)
+		{
+			cout << left[i]<< " ";
+			vecList->push_back(left[i]);
 		}
 	}
+	if (right.size() != iRightCount)
+	{
+		for (int i = iRightCount; i < right.size(); ++i)
+		{
+			cout << right[i];
+			vecList->push_back(right[i]);
+		}
+	}
+
+	cout << endl<<"vecList:";
+	TestPrint(vecList);
 	return vecList;
+}
+
+
+void TestPrint(vector<int>* vec)
+{
+	for (auto it = vec->begin(); it != vec->end(); ++it)
+		cout << *it << " ";
+	cout << endl;
 }
 
 
