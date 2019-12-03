2d84c563bd177457ad4e43bf37f2c8239db2386a
장재완 <wkdwodhks7@naver.com>
2019-12-03 10:54:27 +0900

    구조체 정

--- C++/DataStructure/03HanoiTawel.cpp
+++ C++/DataStructure/03HanoiTawel.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 
 
+void HanoiTowel(int val, char a, char b, char c);
+
+
 void main()
 {
 	// 원반을 A에서 B를 통해 C로 옴김
 	// 한번에 하나만 옴길수 있고  큰 원반이 작은 원반 위로 오면안됨.
-	int arr[3] = { 3, 2, 1 };
-	HanoiTowel(arr);	
+	HanoiTowel(3, 'A', 'B', 'C');	
 	
 }
 
-void HanoiTowel(int arr[])
+void HanoiTowel(int val, char a, char b, char c)
 {
-	
+	if (val == 1)
+	{
+
+	}
+
+	HanoiTowel(val - 1, a, c, b);
+
+	HanoiTowel(val - 1, b, a, c);
 }
